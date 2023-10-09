1e8016dd2ebaefea3abd23a6723b8985cbef8c3d
배성재 <97158957+baeseongjae@users.noreply.github.com>
2023-10-09 23:43:35 +0900

    [Silver I] Title: 회의실 배정, Time: 264 ms, Memory: 54084 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,23 +1,20 @@
 import sys
+
 input = sys.stdin.readline
-
 N = int(input())
-arr = [[0] * 2 for _ in range(N)]
-end = arr[0][1]
-cnt = 0
+arr = [[0, 0] for _ in range(N)]
+answer = 0
 
 for i in range(N):
-    s, e = map(int, input().split())
-    arr[i][0] = s
-    arr[i][1] = e
-    
-# 종료시간이 빠른 순서대로 정렬
-# 종료시간이 같을 경우 시작시간이 빠른순으로 정렬하는 로직도 추가해야함
-arr.sort(key=lambda arr: (arr[1], arr[0]))
+    arr[i][0], arr[i][1] = map(int, input().split())
 
-for i in range(N):
-    if arr[i][0] >= end:
-        end = arr[i][1]
-        cnt += 1
+arr.sort(key=lambda x: (x[1], x[0]))
 
-print(cnt)
+prev = arr[0][1]
+answer = 1
+for i in range(1, N):
+    # 이전 강의시간의 종료시간보다 나중에 시작한다면
+    if arr[i][0] >= prev:
+        prev = arr[i][1]
+        answer += 1
+print(answer)
