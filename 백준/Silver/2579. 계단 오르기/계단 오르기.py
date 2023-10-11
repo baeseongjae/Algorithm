fd2db5b79526f47db6c7936fa32bc07dc8be6c86
배성재 <97158957+baeseongjae@users.noreply.github.com>
2023-10-11 19:26:39 +0900

    [Silver III] Title: 계단 오르기, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,18 +1,19 @@
 N = int(input())
 arr = [0] * (N+1)
 D = [0] * (N+1)
+answer = 0
+
 for i in range(1, N+1):
-    data = int(input())
-    arr[i] = data
+    arr[i] = int(input())
 
-# 계단이 2개이하인 경우
 if N <= 2:
-    print(sum(arr))
-
-# 계단이 3개이상인 경우 (세개 연속해서 못밟으므로)
+    answer = sum(arr)
 else:
     D[1] = arr[1]
     D[2] = arr[1] + arr[2]
+
     for i in range(3, N+1):
-        D[i] = max(D[i-3]+arr[i-1]+arr[i], D[i-2]+arr[i])
-    print(D[N])
+        D[i] = max(D[i-2]+arr[i], D[i-3]+arr[i-1]+arr[i])
+    answer = D[N]
+
+print(answer)
