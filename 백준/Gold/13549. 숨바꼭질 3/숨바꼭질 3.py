c4ffa09b004d0f24cbe34aaa9ee2a5a70f00e13a
배성재 <97158957+baeseongjae@users.noreply.github.com>
2023-12-03 12:32:38 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 132 ms, Memory: 35108 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,32 +1,35 @@
 from collections import deque
+
 N, K = map(int, input().split())
 M = 100000
 dist = [-1] * (M+1)
 
-def BFS(cur):
-    queue = deque([cur])
-    dist[cur] = 0
-    
+def BFS(start):
+    queue = deque([start])
+    dist[start] = 0
+
     while queue:
         cur = queue.popleft()
 
         if cur == K:
-            return dist[cur]
+            return
 
         for next in (cur-1, cur+1, cur*2):
+            # 범위 안에 있고
             if 0 <= next <= M:
+                # 방문하지 않았다면
                 if dist[next] == -1:
+                    # 순간이동이라면
+                    queue.append(next)
                     if next == cur*2:
                         dist[next] = dist[cur]
                     else:
                         dist[next] = dist[cur] + 1
-                    queue.append(next)
-
-                # 방문했었다면
+                # 방문한 노드라면 최솟값 비교
                 else:
                     if next == cur*2:
                         dist[next] = min(dist[next], dist[cur])
                     else:
                         dist[next] = min(dist[next], dist[cur] + 1)
-                
-print(BFS(N))
+BFS(N)
+print(dist[K])
