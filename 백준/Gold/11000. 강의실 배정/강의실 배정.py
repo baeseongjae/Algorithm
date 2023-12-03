68c05335e2f30947a96f85719778b151c206dee2
배성재 <97158957+baeseongjae@users.noreply.github.com>
2023-12-03 13:32:23 +0900

    [Gold V] Title: 강의실 배정, Time: 380 ms, Memory: 74228 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -1,17 +1,15 @@
-import heapq
 import sys
+import heapq
 
 input = sys.stdin.readline
 N = int(input())
-arr = [[0, 0] for _ in range(N)]
-answer = 0 # 강의실 수
+arr = [list(map(int, input().split())) for _ in range(N)]
 heap = []
 
-for i in range(N):
-    arr[i][0], arr[i][1] = map(int, input().split())
-
 arr.sort(key=lambda x: x[0])
-heapq.heappush(heap, arr[0][1]) # 첫 강의 끝나는 시간
+# 처음 강의 끝나는 시간
+heapq.heappush(heap, arr[0][1])
+
 for i in range(1, N):
     # heap[0] 가장 빨리끝나는 강의실임.
     # 다음 강의 시작시간이 더 작으면, 강의실 늘려야하므로
@@ -23,3 +21,5 @@ for i in range(1, N):
         heapq.heappush(heap, arr[i][1])
 
 print(len(heap))
+# 최소의 강의실 사용 (1개가 아님)
+# N개의 수업 => 모두 수업 해야함
