b64452f9ccd7284659dc40817e196f40dde6e087
배성재 <97158957+baeseongjae@users.noreply.github.com>
2023-12-07 23:39:46 +0900

    [Silver III] Title: 바이러스, Time: 60 ms, Memory: 34140 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -6,32 +6,26 @@ input = sys.stdin.readline
 N = int(input())
 E = int(input())
 graph = [[] for _ in range(N+1)]
-visited = [False] * (N+1)
-
+visited = [0] * (N+1)
 for _ in range(E):
     s, e = map(int, input().split())
     graph[s].append(e)
     graph[e].append(s)
 
 #연결된 모든 노드 수 구하면 됨.
-
-def BFS(graph, start):
-    queue = deque()
-    queue.append(start)
+def BFS(start):
+    queue = deque([start])
+    visited[start] = 1
     cnt = 0
 
     while queue:
         cur = queue.popleft()
 
-        # 방문하지 않은 노드라면
-        if not visited[cur]:
-            # 방문!
-            visited[cur] = True
-            cnt += 1
-            for node in graph[cur]:
-                queue.append(node)
+        for next in graph[cur]:
+            if not visited[next]:
+                queue.append(next)
+                cnt += 1
+                visited[next] = 1
+    return cnt
 
-    answer = cnt - 1
-    print(answer)
-
-BFS(graph, 1)
+print(BFS(1))
