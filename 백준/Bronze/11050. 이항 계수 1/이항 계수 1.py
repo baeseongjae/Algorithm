fbd48274e7cb0bebc9b887d40db136fa1d235164
배성재 <97158957+baeseongjae@users.noreply.github.com>
2023-12-07 23:41:54 +0900

    [Bronze I] Title: 이항 계수 1, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -0,0 +1,20 @@
+N, K = map(int, input().split())
+
+# D[i][j] = D[i-1][j-1] + D[i-1][j]
+
+D = [[0 for _ in range(N+1)] for _ in range(N+1)]
+
+
+D[0][0] = 1
+D[0][1] = 0
+
+for i in range(1, N+1):
+    D[0][i] = 0
+    D[i][0] = 1
+    D[i][i] = 1
+
+for i in range(2, N+1):
+    for j in range(1, i):
+        D[i][j] = D[i-1][j-1] + D[i-1][j]
+
+print(D[N][K])
