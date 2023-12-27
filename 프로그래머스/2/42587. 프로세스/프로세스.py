217766355cc3a536181d7d05ef1cb6dedb7667d1
배성재 <97158957+baeseongjae@users.noreply.github.com>
2023-12-27 15:30:13 +0900

    [level 2] Title: 프로세스, Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -4,11 +4,8 @@ def solution(priorities, location):
     answer = 0
     queue = deque([(priority, key) for (key, priority) in enumerate(priorities)])   # 우선순위값과 프로세스번호(인덱스)를 튜플로 묶어 큐에 삽입.
  
-
-    # 큐가 남아있을 동안 pop은 계속됨.
     while queue:
         curPriority, curKey = queue.popleft()   # (우선순위, 프로세스번호)를 pop함.
-        
         # cur와 배열안의 나머지숫자들과 비교.
         if all(curPriority >= item for (item, key) in queue):
             answer += 1
@@ -17,12 +14,3 @@ def solution(priorities, location):
                 return answer
         else:
             queue.append((curPriority, curKey))
-    
-# location에 해당하는 프로세스가 언제 실행되는지 알고싶은거임.
-# priorities에 있는 숫자들은 말그래도 각 프로세스의 우선순위를 의미.
-# 프로세스 번호는 priorities의 인덱스 번호라고 하자.
-# 그럼 큐에 저장을 할때 (key, value) 형태로 저장하면 좋겠지?
-# => key = 인덱스번호, value = 우선순위값
-
-# [1, 1, 9, 1, 1, 1]
-# => [C, D, E, F, A, B]
