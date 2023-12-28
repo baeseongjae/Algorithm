b719ea2ac41ffcb9b6e41f148a3b228ae6c734b0
배성재 <97158957+baeseongjae@users.noreply.github.com>
2023-12-28 16:23:45 +0900

    [level 2] Title: 올바른 괄호, Time: 5.89 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=javascript) 
 
 ### 성능 요약
 
-메모리: 37.1 MB, 시간: 3.31 ms
+메모리: 38.4 MB, 시간: 5.89 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 2일 23:44:27
+2023년 12월 4일 16:23:43
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,19 +1,51 @@
-function solution(s){
-    var answer = true;
-    let stackCnt = 0;
+// function solution(s){
+//     var answer = true;
+//     let stackCnt = 0;
 
-    for (let i = 0; i < s.length ; i++) {
-        if (s[i] === "(") {
-            stackCnt++;
-        }   else if (s[i] === ")") {
-            if (stackCnt <= 0) {
-                answer = false;
-                break;
+//     for (let i = 0; i < s.length ; i++) {
+//         if (s[i] === "(") {
+//             stackCnt++;
+//         }   else if (s[i] === ")") {
+//             if (stackCnt <= 0) {
+//                 answer = false;
+//                 break;
+//             }
+//             stackCnt--;
+//         }
+//     }
+//     if (stackCnt !== 0) answer = false;
+    
+//     return answer;
+// }
+
+
+
+
+
+
+function solution(s) {
+    answer = true;
+    stack = [];
+    
+    for (let x of s) {
+        if (x === '(') {
+            stack.push(x);
+        } else if (x === ')') {
+            // 스택에 아이템이 있으면 팝
+            if (stack.length) {
+                stack.pop();
+            } else {
+                return false;
             }
-            stackCnt--;
         }
     }
-    if (stackCnt !== 0) answer = false;
+    if (stack.length) {
+        return false;
+    }
+    // 괄호 짝이 맞아야 한다.
+    // 근데 ( 이게 먼저 나와야한다.
+    // (()(
+    // 스택을 사용해서 스택에 내용이 없으면 true
     
     return answer;
 }
