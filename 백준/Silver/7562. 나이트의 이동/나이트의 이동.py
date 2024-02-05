6cbea58248a425e84f8c88cf72b85f00af502f6d
배성재 <97158957+baeseongjae@users.noreply.github.com>
2024-02-05 21:08:48 +0900

    [Silver I] Title: 나이트의 이동, Time: 1228 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -1,38 +1,33 @@
 from collections import deque
 
-def BFS(curX, curY, cnt):
+T = int(input())
+d = [(-2, -1), (-1, -2), (-2, 1), (-1, 2), (1, 2), (2, 1), (1, -2), (2, -1)]
+
+
+def bfs(sx, sy):
     queue = deque()
-    queue.append(((curX, curY), cnt))
-    visited[curX][curY] = 1
-    
+    queue.append([(sx, sy), 0])
+    visited[sx][sy] = 1
+
     while queue:
         (curX, curY), cnt = queue.popleft()
 
-        if curX == desX and curY == desY:
-            print(cnt)
-            return
-        
-        for i in range(8):
-            nx = curX + dx[i]
-            ny = curY + dy[i]
-            if 0 <= nx < I and 0 <= ny < I and not visited[nx][ny]:
-                queue.append(((nx, ny), cnt+1))
+        if curX == endX and curY == endY:
+            return cnt
+
+        for (dx, dy) in d:
+            nx = curX + dx
+            ny = curY + dy
+
+            if 0 <= nx < l and 0 <= ny < l and not visited[nx][ny]:
+                queue.append([(nx, ny), cnt + 1])
                 visited[nx][ny] = 1
 
-T = int(input())
+for i in range(T):
+    l = int(input())
+    visited = [[0 for _ in range(l)] for _ in range(l)]
 
-for _ in range(T):
-    I = int(input())
-    curX, curY = map(int, input().split())
-    desX, desY = map(int, input().split())
-    
-    visited = [[0 for _ in range(I)] for _ in range(I)]
-    dx = [-2, -2, -1, -1, 1, 1, 2, 2]
-    dy = [-1, 1, -2, 2, -2, 2, -1, 1]
-    cnt = 0
+    startX, startY = map(int, input().split())
+    endX, endY = map(int, input().split())
 
-    BFS(curX, curY, cnt)
-
-
-# 체스에서 나이트의 능력대로 이동하면서 목표지점까지의 최소 연산횟수를 구하는 문제
-# BFS
+    print(bfs(startX, startY))
